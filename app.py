746990a961ee9f9927cebdaca7805da89d1e0636
Arbaz Raza <arbazraza2002@gmail.com>
2025-04-18 00:05:59 +0530

    done

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def predict():
     except Exception as e:
         return render_template('index.html', result=f"Error: {e}")
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
